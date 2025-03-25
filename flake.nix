36fd270f9622df14921c410938e2c921f2f89b1b
Break Yang <breakds@gmail.com>
2025-03-25 12:22:13 -0700

    Add iterm2

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
-        emacs
+        iterm2
         git
         lsd
         bat
