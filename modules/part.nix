f9b9897706065976d5c3ac8ca49809644151792c
Break Yang <breakds@gmail.com>
2025-03-28 09:50:16 -0700

    Update

--- modules/part.nix
+++ modules/part.nix
@@ -11,26 +11,28 @@ in {
     
     # TODO: Also import nix-homebrew here.
     homebrew = { pkgs, ... }: {
-      nix-homebrew = {
-        enable = true;
-        enableRosetta = true;
+      config = {
+        nix-homebrew = {
+          enable = true;
+          enableRosetta = true;
 
-        taps = {
-          "homebrew/homebrew-core" = inputs.homebrew-core;
-          "homebrew/homebrew-cask" = inputs.homebrew-cask;
-          "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+          taps = {
+            "homebrew/homebrew-core" = inputs.homebrew-core;
+            "homebrew/homebrew-cask" = inputs.homebrew-cask;
+            "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
+          };
+
+          # Fully declarative tap management. Taps can no longer be added
+          # imperatively with `brew tap`.
+          mutableTaps = false;
         };
-
-        # Fully declarative tap management. Taps can no longer be added
-        # imperatively with `brew tap`.
-        mutableTaps = false;
-      };
-      
-      homebrew = {
-        enable = true;
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
+        
+        homebrew = {
+          enable = true;
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
       };
     };
   };
